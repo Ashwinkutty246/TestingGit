48973b701869b718977a681734a14ec2c1c22838
prathap2904 <prathap2904@gmail.com>
2024-12-04 11:41:45 +0530

    first comit

---
+++ src/main/java/com/TestingDemo/Tetsing/TetsingApplication.java
@@ -0,0 +1,13 @@
+package com.TestingDemo.Tetsing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetsingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetsingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TestingDemo/Tetsing/TetsingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TestingDemo.Tetsing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetsingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
